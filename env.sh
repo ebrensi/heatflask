4a9713e1f0bd2054abb5851f0003bde7ec90e4e1
Efrem <BarefootEfrem@gmail.com>
2016-08-23 19:08:05 -0700

    a bunch of changes.  bad git habits I know

--- env.sh
+++ env.sh
@@ -3,6 +3,10 @@
 source activate runapp
 
 export DATABASE_URL=postgresql://heatmapp:heatmapp@localhost/heatmapp
+export SERVER_NAME=localhost
 export FLASK_APP=heatmapp.py
 export FLASK_DEBUG=1
+
+export STRAVA_CLIENT_ID="12700"
+export STRAVA_CLIENT_SECRET="04d0fffe327fa71bffcbb4c9bc00c26a0d530e4b"
 
