47ffcf2d6f2945fa2687adb8faba0ecf1fe665a4
Efrem <BarefootEfrem@gmail.com>
2016-08-30 13:04:54 -0700

    switched to using stravalib instead of flask-oauthlib

--- strava.py
+++ strava.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
 from flask import Flask, redirect, url_for, session, request, jsonify
-from flask_oauthlib.client import OAuth
+from stravalib import Client
 
 STRAVA_CLIENT_ID = "12700"
 STRAVA_CLIENT_SECRET = "04d0fffe327fa71bffcbb4c9bc00c26a0d530e4b"
@@ -13,25 +13,20 @@ SECRET_KEY = "secret"
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-oauth = OAuth(app)
-
-strava = oauth.remote_app(
-    'strava',
-    base_url='https://www.strava.com/api/v3/',
-    request_token_url=None,
-    access_token_url=app.config["STRAVA_TOKEN_URL"],
-    authorize_url=app.config["STRAVA_AUTH_URL"],
-    consumer_key=app.config["STRAVA_CLIENT_ID"],
-    consumer_secret=app.config["STRAVA_CLIENT_SECRET"],
-    access_token_method='POST'
-)
+client = Client()
 
 
 @app.route('/')
 def index():
-    if 'strava_token' in session:
-        me = strava.get('athlete')
-        return jsonify(me.data)
+    if client.access_token:
+        athlete = client.get_athlete()
+
+        ath = {"id": athlete.id,
+               "firstname": athlete.firstname,
+               "lastname": athlete.lastname,
+               "username": athlete.username
+               }
+        return jsonify(ath)
     else:
         return redirect(url_for('login'))
 
@@ -39,50 +34,60 @@ def index():
 @app.route('/login')
 def login():
     redirect_uri = url_for('authorized', _external=True)
-    return strava.authorize(callback=redirect_uri,
-                            approval_prompt="force")
+    auth_url = client.authorization_url(client_id=app.config["STRAVA_CLIENT_ID"],
+                                        redirect_uri=redirect_uri,
+                                        approval_prompt="force")
+    return redirect(auth_url)
 
 
-@app.route('/logout')
-def logout():
-    session.pop('strava_token', None)
-    return redirect(url_for('index'))
+# @app.route('/logout')
+# def logout():
+#     session.pop('strava_token', None)
+#     return redirect(url_for('index'))
 
 
 @app.route('/login/authorized')
 def authorized():
-    resp = strava.authorized_response()
+    code = request.args['code']
+    access_token = client.exchange_code_for_token(client_id=app.config["STRAVA_CLIENT_ID"],
+                                                  client_secret=app.config[
+                                                      "STRAVA_CLIENT_SECRET"],
+                                                  code=code)
 
-    if resp is None:
+    if not access_token:
         return ('Access denied: reason={} error={}'
                 .format(request.args['error'],
                         request.args['error_description']))
 
     else:
-        session['strava_token'] = (resp['access_token'], '')
-        me = strava.get('athlete')
-        return jsonify(me.data)
+        client.access_token = access_token
+        return redirect(url_for("index"))
 
 
 @app.route('/activities')
 def activities():
-    if 'strava_token' in session:
-        page = request.args["page"]
-        data = {"page": page,
-                "per_page": 50}
-        activities = strava.get('athlete/activities', data=data).data
-        a_list = {a["id"]: "{}: {}".format(a["name"], a["start_date"])
-                  for a in activities}
-        a_dict = {"activities": a_list,
-                  "count": len(a_list)}
-        return jsonify(a_dict)
+    if client.access_token:
+        act = client.get_activities(after="2010-01-01T00:00:00Z")
+        ids = {a.id: a.name for a in act}
+        return jsonify(ids)
+
+    # if 'strava_token' in session:
+    #     page = request.args["page"]
+    #     data = {"page": page,
+    #             "per_page": 50}
+    #     activities = strava.get('athlete/activities', data=data).data
+    #     a_list = {a["id"]: "{}: {}".format(a["name"], a["start_date"])
+    #               for a in activities}
+    #     a_dict = {"activities": a_list,
+    #               "count": len(a_list)}
+    #     return jsonify(a_dict)
     else:
         return redirect(url_for('login'))
 
 
-@strava.tokengetter
-def get_strava_token():
-    return session.get('strava_token')
+# @strava.tokengetter
+# def get_strava_token():
+#     return session.get('strava_token')
 
 
 if __name__ == '__main__':
