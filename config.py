530c84f469d8dba1572cf80ff112187c263ba29d
Efrem <BarefootEfrem@gmail.com>
2016-08-31 18:39:06 -0700

    integrated strava.py into heatmapp.py

--- config.py
+++ config.py
@@ -10,6 +10,11 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "pB\xeax\x9cJ\xd6\x81\xed\xd7\xf9\xd0\x99o\xad\rM\x92\xb1\x8b{7\x02r"
 
+    STRAVA_CLIENT_ID = os.environ["STRAVA_CLIENT_ID"]
+    STRAVA_CLIENT_SECRET = os.environ["STRAVA_CLIENT_SECRET"]
+    STRAVA_AUTH_URL = "https://www.strava.com/oauth/authorize"
+    STRAVA_TOKEN_URL = "https://www.strava.com/oauth/token"
+
 
 class ProductionConfig(Config):
     DEBUG = False
