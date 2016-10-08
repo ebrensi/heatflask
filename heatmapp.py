bab8fee19bf7b6064f62d5b20b9771b479c6a253
Efrem <BarefootEfrem@gmail.com>
2016-10-08 15:27:00 -0700

    add caching for demo view

--- heatmapp.py
+++ heatmapp.py
@@ -11,9 +11,12 @@ import os
 import stravalib
 import flask_login
 from flask_login import current_user, login_user, logout_user, login_required
+from flask_caching import Cache
+
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 # views will be sent as gzip encoded
 flask_compress.Compress(app)
@@ -46,6 +49,7 @@ def nothing():
 
 
 @app.route("/login")
+@cache.cached(timeout=120)
 def login():
     return render_template("login.html")
 
@@ -141,6 +145,7 @@ def delete(username):
 
 
 @app.route('/demo')
+@cache.cached(timeout=50)
 def demo():
     return redirect(url_for("index",
                             username="ebuggz",
