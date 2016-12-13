bd927d7cb2435c917901d198b3f45f0efbdf6877
Efrem <BarefootEfrem@gmail.com>
2016-12-12 20:02:21 -0800

    fixes backwards index sorting

--- heatmapp.py
+++ heatmapp.py
@@ -30,7 +30,7 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 
 # set up short-term fast caching support
 cache = flask_caching.Cache(app)
-# cache.clear()
+cache.clear()
 
 # models depend on cache and app so we import them afterwards
 from models import User, db, inspector
