09a31fb3c0acc20769237c9748bc66636b5ded27
Efrem <BarefootEfrem@gmail.com>
2017-02-19 12:07:11 -0800

    separates out dotlayer defs

--- static/js/L.dotLayer.js
+++ static/js/L.dotLayer.js
@@ -1,64 +1,103 @@
 /* Canvas Layer */
-  DELAY_CONST = 100;
+DELAY_CONST = 100;
 
-  function drawDots(info, A, time) {
-      if (!info.bounds.intersects(A.bounds)) {
-          return;
-      }
+function drawDots(info, A, time) {
+    if (!info.bounds.intersects(A.bounds)) {
+        return;
+    }
 
-      const times = A.time,
-            latlngs = A.latlng,
-            max_time = times[times.length-1],
-            zoom = info.zoom,
-            delay = DELAY_CONST/Math.sqrt(zoom),
-            num_pts = Math.floor(max_time / delay),
-            ctx = info.canvas.getContext('2d');
+    const times = A.time,
+          latlngs = A.latlng,
+          max_time = times[times.length-1],
+          zoom = info.zoom,
+          delay = DELAY_CONST/Math.sqrt(zoom),
+          num_pts = Math.floor(max_time / delay),
+          ctx = info.canvas.getContext('2d');
 
-      let s = time % max_time,
-          key_time = s - delay * Math.floor(s/delay);
+    let s = time % max_time,
+        key_time = s - delay * Math.floor(s/delay);
 
-      let i=0, t, d, dt, p1, p2, p, size, interval_good;
-      // ctx.fillStyle = "#FFFFFF";
-      if (A.selected) {
-          size = 4;
-          ctx.fillStyle = "#FFFFFF";
-      } else {
-          size = 2;
-          ctx.fillStyle = "#000000";
-      }
+    let i=0, t, d, dt, p1, p2, p, size, interval_good;
+    // ctx.fillStyle = "#FFFFFF";
+    if (A.selected) {
+        size = 4;
+        ctx.fillStyle = "#FFFFFF";
+    } else {
+        size = 2;
+        ctx.fillStyle = "#000000";
+    }
 
-      for (let j = 0; j < num_pts; j++) {
-          t = (key_time + j*delay);
-          if (t >= times[i]) {
-            while (t >= times[i]) {
-              i++;
-            }
-
-            p1 = latlngs[i-1];
-            p2 = latlngs[i];
-            interval_good = info.bounds.contains(p1) || info.bounds.contains(p2);
-
-            if (interval_good) {
-                dt = times[i] - times[i-1];
-                M = [(p2[0]-p1[0])/dt,  (p2[1]-p1[1])/dt];
-            }
+    for (let j = 0; j < num_pts; j++) {
+        t = (key_time + j*delay);
+        if (t >= times[i]) {
+          while (t >= times[i]) {
+            i++;
           }
+
+          p1 = latlngs[i-1];
+          p2 = latlngs[i];
+          interval_good = info.bounds.contains(p1) || info.bounds.contains(p2);
 
           if (interval_good) {
-
-              dt = t - times[i-1];
-              p = [p1[0] + M[0]*dt, p1[1] + M[1]*dt];
-
-              if (info.bounds.contains(p)) {
-                  dot = info.layer._map.latLngToContainerPoint(p);
-                  // ctx.fillRect(dot.x-1, dot.y-1, size, size);
-                  ctx.beginPath();
-                  ctx.arc(dot.x, dot.y, size, 0, Math.PI * 2);
-                  ctx.fill();
-                  ctx.closePath();
-              }
+              dt = times[i] - times[i-1];
+              M = [(p2[0]-p1[0])/dt,  (p2[1]-p1[1])/dt];
           }
+        }
 
-      }
+        if (interval_good) {
 
-  }
+            dt = t - times[i-1];
+            p = [p1[0] + M[0]*dt, p1[1] + M[1]*dt];
+
+            if (info.bounds.contains(p)) {
+                dot = info.layer._map.latLngToContainerPoint(p);
+                // ctx.fillRect(dot.x-1, dot.y-1, size, size);
+                ctx.beginPath();
+                ctx.arc(dot.x, dot.y, size, 0, Math.PI * 2);
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+
+    }
+
+}
+
+/* for dot paths */
+function onDrawLayer(info) {
+    let now = Date.now();
+
+    fps_display.update();
+
+    let ctx = info.canvas.getContext('2d'),
+        time = (now - this.start_time) >>> 6;
+
+    ctx.clearRect(0, 0, info.canvas.width, info.canvas.height);
+
+    let ids = Object.keys(appState.items);
+    for (i = 0; i < ids.length; i++) {
+        let A = appState.items[ids[i]];
+        if (("time" in A) && ("latlng" in A)) {
+            drawDots(info, A, time)
+        } else {
+            console.log(A);
+        }
+    }
+};
+
+function animate() {
+  this.paused = false;
+  this.start_time = Date.now();
+  L.Util.requestAnimFrame(this._animate, this);
+}
+
+function pause() {
+  this.paused = true;
+}
+
+function _animate() {
+    if (!this.paused) {
+      this.drawLayer();
+      L.Util.requestAnimFrame(this._animate, this);
+    }
+}
