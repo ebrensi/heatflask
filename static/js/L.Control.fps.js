bf533205a80cefd985a75b41048cc56bcb223537
Efrem <BarefootEfrem@gmail.com>
2017-02-19 10:45:14 -0800

    separates out L.Control.fps

---
+++ static/js/L.Control.fps.js
@@ -0,0 +1,30 @@
+/* fps display control for leaflet*/
+/* Efrem Rensi 2017/2/19 */
+
+L.Control.fps = L.Control.extend({
+    lastCalledTime: 1,
+
+    options: {
+        position: "topright"
+    },
+
+    onAdd: function (map) {
+        // Control container
+        this._container = L.DomUtil.create('div', 'leaflet-control-fps');
+        L.DomEvent.disableClickPropagation(this._container);
+        this.update(0);
+        return this._container;
+    },
+
+    update: function(now=Date.now()) {
+        let fps = 100 / (now - this.lastCalledTime);
+        this._container.innerHTML = "FPS: " + 10 * Math.floor(fps);
+        this.lastCalledTime = now;
+        return fps
+  }
+});
+
+//constructor registration
+L.control.fps = function(options) {
+  return new L.Control.fps();
+};
