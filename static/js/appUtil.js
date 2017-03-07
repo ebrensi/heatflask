de344e2192529d2989d5f0abb560966b3c70ab25
Efrem <BarefootEfrem@gmail.com>
2017-03-07 13:27:33 -0800

    updates webhooks

--- static/js/appUtil.js
+++ static/js/appUtil.js
@@ -23,3 +23,18 @@ function formatIP(data, type, row, meta) {
         return "";
     }
 }
+
+
+function stravaActivityURL(id) {
+    return `https://www.strava.com/activities/${id}`;
+}
+
+
+function hhmmss(secs) {
+    return new Date(secs * 1000).toISOString().substr(11, 8);
+}
+
+
+function img(url, w=20, h=20){
+  return `<img src=${url} width=${w} height=${h} class="img-fluid">`;
+}
