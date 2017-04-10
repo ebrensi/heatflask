45feea67d488ec1df26040d02559b44caa129cac
Efrem <BarefootEfrem@gmail.com>
2017-04-10 10:09:55 -0700

    jscs formatting

--- static/js/appUtil.js
+++ static/js/appUtil.js
@@ -1,41 +1,39 @@
 
-function stravaActivityURL(id) {
+function stravaActivityURL( id ) {
     return `https://www.strava.com/activities/${id}`;
 }
 
-function stravaAthleteURL(id) {
-    return `https://www.strava.com/athletes/${id}`
+function stravaAthleteURL( id ) {
+    return `https://www.strava.com/athletes/${id}`;
 }
 
-function href(url, text){
-    return `<a href='${url}' target='_blank'>${text}</a>`
+function href( url, text ) {
+    return `<a href='${url}' target='_blank'>${text}</a>`;
 }
 
-function ip_lookup_url(ip) {
-    return ip ? `http://freegeoip.net/json/${ip}`: "#"
+function ip_lookup_url( ip ) {
+    return ip ? `http://freegeoip.net/json/${ip}` : "#";
 }
 
-
-
-function formatDate(data, type, row, meta) {
-    date = new Date(data);
-    return (type === "display" || type === "filter") ?
-        date.toLocaleString('en-US', { hour12: false }) : date;
+function formatDate( data, type, row, meta ) {
+    date = new Date( data );
+    return ( type === "display" || type === "filter" ) ?
+        date.toLocaleString( "en-US", { hour12: false } ) : date;
 }
 
-function formatIP(data, type, row, meta) {
-    if (data) {
+function formatIP( data, type, row, meta ) {
+    if ( data ) {
         let ip = data;
-        return (type === "display") ? href(ip_lookup_url(ip), ip): ip;
+        return ( type === "display" ) ? href( ip_lookup_url( ip ), ip ) : ip;
     } else {
         return "";
     }
 }
 
-function formatUserId (data, type, row) {
-    if (data) {
-        if (type == "display") {
-            return href("/" + data, img(row.profile, w=40, h=40, alt=data));
+function formatUserId ( data, type, row ) {
+    if ( data ) {
+        if ( type == "display" ) {
+            return href( "/" + data, img( row.profile, w = 40, h = 40, alt = data ) );
         } else {
             return data;
         }
@@ -44,12 +42,10 @@ function formatUserId (data, type, row) {
     }
 }
 
-
-function hhmmss(secs) {
-    return new Date(secs * 1000).toISOString().substr(11, 8);
+function hhmmss( secs ) {
+    return new Date( secs * 1000 ).toISOString().substr( 11, 8 );
 }
 
-
-function img(url, w=20, h=20, alt=""){
+function img( url, w=20, h=20, alt="" ) {
   return `<img src=${url} width=${w} height=${h} class="img-fluid" alt="${alt}">`;
 }
