49fd360f298b5f8e40fe1f4fd1684e5857bc817b
Efrem <BarefootEfrem@gmail.com>
2017-05-03 18:44:29 -0700

    Float32Array implementation works great now

--- config.py
+++ config.py
@@ -104,7 +104,7 @@ class DevelopmentConfig(Config):
     These are settings specific to the development environment
     (Developer's personal computer)
     """
-    OFFLINE = False
+    OFFLINE = True
     DEVELOPMENT = True
     DEBUG = True
     CACHE_ACTIVITIES_TIMEOUT = 2 * 60 * 60
