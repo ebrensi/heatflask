ecddeb49a88250fe50e0e69ea788ed68b9cb8ca1
Efrem <BarefootEfrem@gmail.com>
2017-05-24 10:45:42 -0700

    removes pool limit

--- models.py
+++ models.py
@@ -234,7 +234,8 @@ class Users(UserMixin, db_sql.Model):
                 # )
                 return data if data else user
 
-            P = Pool(app.config["CONCURRENCY"])
+            # P = Pool(app.config["CONCURRENCY"])
+            P = Pool()
             try:
                 for obj in P.imap_unordered(user_data, cls.query):
                     if type(obj) == cls:
