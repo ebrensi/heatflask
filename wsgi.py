81170cef8540bcffcad0a64bb52ed79b664e10c0
Efrem <BarefootEfrem@gmail.com>
2019-11-06 15:39:12 -0800

    fix logging

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,12 @@
 from heatflask import create_app
 from signal import signal, SIGPIPE, SIG_DFL
+import logging
 
 app = create_app()
-log = app.logger
+
+gunicorn_logger = logging.getLogger('gunicorn.error')
+app.logger.handlers = gunicorn_logger.handlers
+app.logger.setLevel(gunicorn_logger.level)
 
 loc_status = ""
 if app.config.get("OFFLINE"):
@@ -10,6 +14,7 @@ if app.config.get("OFFLINE"):
 elif app.config.get("USE_REMOTE_DB"):
     loc_status = ": USING REMOTE DATA-STORES"
 
+log = app.logger
 log.info("Heatflask server starting{}".format(loc_status))
 
 # makes python ignore sigpipe and prevents broken pipe exception when client
