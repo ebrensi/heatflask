ffdfd58ea37ee20f72993e6ab40ef11a22c0a799
Efrem <BarefootEfrem@gmail.com>
2019-11-25 13:36:33 -0800

    make logging level settable in environment as LOG_LEVEL

--- wsgi.py
+++ wsgi.py
@@ -11,29 +11,25 @@ log = app.logger
 gunicorn_logger = logging.getLogger('gunicorn.error')
 handlers = gunicorn_logger.handlers
 
-app.logger.handlers = gunicorn_logger.handlers
-app.logger.setLevel(gunicorn_logger.level)
-
 for handler in handlers:
     formatter = handler.setFormatter(logging.Formatter(
         '[%(process)d] [%(levelname)s] %(message)s'
     ))
 
+app.logger.handlers = handlers
+log_level_name = app.config["LOG_LEVEL"]
+log_level = logging.getLevelName(log_level_name)
+app.logger.setLevel(log_level)
+
 loc_status = ""
 if app.config.get("OFFLINE"):
-    loc_status = ": OFFLINE"
+    loc_status = " OFFLINE"
 elif app.config.get("USE_REMOTE_DB"):
-    loc_status = ": USING REMOTE DATA-STORES"
+    loc_status = " USING REMOTE DATA-STORES"
 
-
-log.info("Heatflask server starting{}".format(loc_status))
+log.info("Heatflask server starting%s LOG_LEVEL=%s", loc_status, log_level_name)
 
 # makes python ignore sigpipe and prevents broken pipe exception when client
 #  aborts an SSE stream by closing the browser window
 signal(SIGPIPE, SIG_DFL)
 
-# if __name__ == '__main__':
-#     from gevent import pywsgi
-#     from geventwebsocket.handler import WebSocketHandler
-#     server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
-#     server.serve_forever()
