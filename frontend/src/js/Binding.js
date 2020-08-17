20cf9772e767e1094d392e2b398efee4a9886c69
Efrem-Asus <BarefootEfrem@gmail.com>
2020-08-17 13:55:40 -0700

    improve Binding module

--- frontend/src/js/Binding.js
+++ frontend/src/js/Binding.js
@@ -1,5 +1,9 @@
-const identity = x => x;
+/**
+ * Lightweight data-binding
+ * @module
+ */
 
+const identity = (x) => x;
 
 /**
  * A BoundVariable object has a value that can be bound to a DOM elelment
@@ -34,10 +38,11 @@ export class BoundVariable {
    */
   set(newValue) {
     this._value = newValue;
+    // console.log(newValue);
 
     for (let i = 0; i < this.countDB; i++) {
       const binding = this.DOMbindings[i];
-      binding.element[binding.attribute] =  binding.format(newValue);
+      binding.element[binding.attribute] = binding.format(newValue);
     }
 
     for (let i = 0; i < this.countGB; i++) {
@@ -65,23 +70,32 @@ export class BoundVariable {
   /**
    * Sync this value with an attribute of a DOM element.
    *
-   * @param element  the DOM element
-   * @param attribute the attribute to sync (eg. "value", "checked", "href", "innerHTML", etc.)
-   * @param event the event on which to update the value. (eg. "change", "keyup", etc)
+   * @param {Object} args - keyword arguments in an Object
+   * @param {HTMLElement} args.element - The DOM element
+   * @param {String} [args.attribute="value"] - The attribute to sync
+   *                 (eg. "value", "checked", "href", "innerHTML", etc.)
+   * @param {String} [args.event] - The event on which to update the value
+   *         (eg. "change", "keyup", etc). "none" is the same as not having an event.
+   * @param {function} [args.DOMformat] - A function that converts the varible value
+   *                                    to be displayed in the DOM.
+   * @param {function} [args.varFormat] - A function that converts the DOM value to
+   *                                    the variable value.
    */
-  addDOMbinding({element, attribute, DOMformat, event, varFormat}) {
+  addDOMbinding({ element, attribute, DOMformat, event, varFormat }) {
     const binding = {
       element: element,
       attribute: attribute,
-      format: DOMformat || identity
+      format: DOMformat || identity,
     };
 
-    if (event) {
+    if (event && event !== "none") {
       const format = varFormat || identity;
 
       element.addEventListener(
         event,
-        function () { this.set( format(element[attribute])) }.bind(this)
+        function () {
+          this.set(format(element[attribute]));
+        }.bind(this)
       );
     }
 
@@ -96,27 +110,21 @@ export class BoundVariable {
   /**
    * Add binding to a generally defined remote value.
    * Use this to sync this value with a general variable, given a
-   * function to update that variable.
+   * function to update that variable. !! The user is responsible for
+   * updating this variable when the remote value changes !!
    *
-   * @param {function} setFunc A function that updates the remote value when this one changes.
+   * @param {function} setFunc - A function that updates the remote value
+   *                           when this one changes.
    */
-  addGeneralBinding(setFunc, DOMformat) {
-    const binding = {
-      setFunc: setFunc,
-      format: DOMformat || identity
-    };
-
-    this.generalBindings.push(binding);
+  addGeneralBinding(setFunc) {
+    this.generalBindings.push(setFunc);
     this.countGB = this.generalBindings.length;
 
     setFunc(this._value);
 
-    return function onChange(newVal) {
-      this.set(newVal);
-    };
+    return this
   }
 }
-
 
 /**
  * An object with {@link BoundVariable}s as properties.
@@ -134,20 +142,22 @@ export class BoundVariable {
  *
  */
 export class BoundVariableCollection extends Object {
+  constructor(...args) {
+    super(...args);
+    this.binds = {};
+  }
+
   /**
    * Add a {@link BoundVariable} object
    * @param {String|Number} key The "property" associated with this {@BoundVariable}
    * @param {[type]} bv The {@link BoundVariable} object.
    */
   add(key, bv) {
-    if (!this.binds) {
-      this.binds = {};
-    }
     this.binds[key] = bv;
 
     Object.defineProperty(this, "binds", {
-      enumerable: false
-    })
+      enumerable: false,
+    });
 
     Object.defineProperty(this, key, {
       get: function () {
@@ -158,9 +168,8 @@ export class BoundVariableCollection extends Object {
       }.bind(this),
 
       enumerable: true,
-
     });
 
-    return bv
+    return bv;
   }
 }
