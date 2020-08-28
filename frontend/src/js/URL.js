6718f100b02aea58846e8695a6b911d2649c168b
Efrem-Asus <BarefootEfrem@gmail.com>
2020-08-28 01:31:06 -0700

    Improve query UI and URL update

--- frontend/src/js/URL.js
+++ frontend/src/js/URL.js
@@ -4,73 +4,105 @@
 
 import { qParams, vParams, urlArgDefaults } from "./Model.js";
 // import { dotLayer } from "../DotLayerAPI.js";
-import { defaultBaselayerName, map } from "./MapAPI.js";
+import { defaultBaselayerName, map, sidebar } from "./MapAPI.js";
 
-let url;
+/*
+ * qArgs are updated on render and vArgs are updated every time the map moves
+ */
+let url, qArgs;
 
-function updateURL() {
-  const args = {};
-
+function initializeURL() {
+  qArgs = {};
   switch (qParams.queryType) {
     case "activities":
-      args.n = qParams.quantity;
+      qArgs.n = qParams.quantity;
       break;
 
     case "days":
-      args.days = qParams.quantity;
+      qArgs.days = qParams.quantity;
       break;
 
     case "ids":
-      args.id = qParams.ids;
+      qArgs.id = qParams.ids;
       break;
 
     case "dates":
-      if (qParams.before) args.before = qParams.before;
-      if (qParams.after) args.after = qParams.after;
+      if (qParams.before) qArgs.before = qParams.before;
+      if (qParams.after) qArgs.after = qParams.after;
       break;
 
     case "key":
-      args.key = qParams.key;
+      qArgs.key = qParams.key;
   }
 
+  if (qParams.userid) qArgs.user = qParams.userid;
+
+  updateURL();
+}
+
+/*
+ * Reset qParams back to the values from the last render (in qArgs).
+ */
+export function resetQuery() {
+  if ("n" in qArgs) {
+    qParams.queryType = "activities";
+    qParams.quantity = qArgs.n;
+  } else if ("days" in qArgs) {
+    qParams.queryType = "days";
+    qParams.quantity = qArgs.days;
+  } else if ("id" in qArgs) {
+    qParams.queryType = "ids";
+    qParams.ids = qArgs.id;
+  } else if ("before" in qArgs) {
+    qParams.queryType = "dates";
+    qParams.before = qArgs.before;
+    qParams.after = qArgs.after;
+  } else if ("key" in qArgs) {
+    qParams.queryType = "key";
+    qParams.key = qArgs.key;
+  }
+}
+
+/*
+ * If the user closes the sidebar without performing the query, the
+ * query form resets to the last query.
+ */
+sidebar.addEventListener("closing", resetQuery);
+
+function updateURL() {
+  const vArgs = {};
+
   // put geohash in the url if autozoom is disabled
-  if (!vParams.autozoom) args.geohash = vParams.geohash;
+  if (!vParams.autozoom) vArgs.geohash = vParams.geohash;
 
   // include baselayer name if it isn't the default
-  if (vParams.baselayer !== defaultBaselayerName) args.map = vParams.baselayer;
+  if (vParams.baselayer !== defaultBaselayerName) vArgs.map = vParams.baselayer;
 
   // include boolean options as 1 or 0 if they differ from the defaults
   for (const param of ["paused", "shadows", "paths"]) {
     const val = vParams[param],
       defaultVal = urlArgDefaults[param][1];
     if (val !== defaultVal) {
-      args[param] = val ? 1 : 0;
+      vArgs[param] = val ? 1 : 0;
     }
   }
 
   // include the global alpha value if it differs from 1
-  if (vParams.alpha < 1) args.alpha = vParams.alpha;
+  if (vParams.alpha < 1) vArgs.alpha = vParams.alpha;
 
-  if (qParams.userid)
-    args.user = qParams.userid;
-
-  const paramsString = Object.entries(args)
+  const paramsString = Object.entries({ ...qArgs, ...vArgs })
       .map(([k, v]) => `${k}=${encodeURIComponent(v)}`)
       .join("&"),
     // newURL = `${targetUser.id}?${paramsString}`;
     newURL = `?${paramsString}`;
 
   if (url !== newURL) {
-    console.log(`pushing: ${newURL}`);
+    // console.log(`pushing: ${newURL}`);
     url = newURL;
     window.history.replaceState("", "", newURL);
   }
 }
 
-
-
-updateURL();
-
-qParams.onChange(updateURL);
+initializeURL();
 vParams.onChange(updateURL);
 map.on("moveend", updateURL);
