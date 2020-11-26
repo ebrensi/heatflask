e79ca0ce25a4ac17037799d94dd8b2ae87cf03eb
Efrem-Asus <BarefootEfrem@gmail.com>
2020-11-26 11:45:46 -0800

    add IQR based outiler filter algorithm

--- frontend/src/js/appUtil.js
+++ frontend/src/js/appUtil.js
@@ -128,3 +128,33 @@ export function histogram(points, bins) {
   // console.log(bins)
   return binCounts
 }
+
+/**
+ * Filters outliers from an Array using standard IQR method. Creates a new Array.
+ * @param  {Array} someArray
+ * @return {Array}
+ */
+export function filterOutliers(someArray, mult = 1.5) {
+  if (someArray.length < 4) return someArray
+
+  let values, q1, q3, iqr, maxValue, minValue
+
+  values = someArray.slice().sort((a, b) => a - b) //copy array fast and sort
+
+  if ((values.length / 4) % 1 === 0) {
+    //find quartiles
+    q1 = (1 / 2) * (values[values.length / 4] + values[values.length / 4 + 1])
+    q3 =
+      (1 / 2) *
+      (values[values.length * (3 / 4)] + values[values.length * (3 / 4) + 1])
+  } else {
+    q1 = values[Math.floor(values.length / 4 + 1)]
+    q3 = values[Math.ceil(values.length * (3 / 4) + 1)]
+  }
+
+  iqr = q3 - q1
+  maxValue = q3 + iqr * mult
+  minValue = q1 - iqr * mult
+
+  return values.filter((x) => x >= minValue && x <= maxValue)
+}
