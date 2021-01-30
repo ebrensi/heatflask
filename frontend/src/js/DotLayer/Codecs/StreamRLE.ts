27d53efdef5dd698e30e5017a46844bee50e486a
Efrem-Toshiba <barefootefrem@gmail.com>
2021-01-29 17:56:03 -0800

    clean up StreamRLE and unit test

--- frontend/src/js/DotLayer/Codecs/StreamRLE.ts
+++ frontend/src/js/DotLayer/Codecs/StreamRLE.ts
@@ -28,13 +28,14 @@ function* cumulativeSum(
   firstValue?: number
 ): IterableIterator<number> {
   let sum = firstValue || 0
+  yield sum
   for (const num of arr) yield (sum += num)
 }
 
 /**
  * The number of values in the original (non RLE-encoded) list
  */
-export function decodedList1Length(rleList: RLElist1): number {
+export function decode1Length(rleList: RLElist1): number {
   let len = 0 // We don't count the start value!
   for (const el of rleList) {
     if (el instanceof Array) len += el[1]
@@ -46,7 +47,7 @@ export function decodedList1Length(rleList: RLElist1): number {
 /**
  * Decode RLE list
  */
-export function* decodeList1(
+export function* decode1(
   rleList: RLElist1,
   exclude?: anySet
 ): IterableIterator<number> {
@@ -71,19 +72,19 @@ export function* decodeList1(
  * Decode a (RLE-encoded as a List) array of successive differences
  * into the original values
  */
-export function* decodeDiffList1(
+export function decode1Diffs(
   rleList: RLElist1,
   firstValue?: number,
   exclude?: anySet
 ): IterableIterator<number> {
-  const decodedDiffs = decodeList1(rleList, exclude)
-  return cumulativeSum(decodedDiffs, firstValue)
+
+  return cumulativeSum(decode1(rleList, exclude), firstValue)
 }
 
 /**
  * Transcode RLElist into alternate form
  */
-export function* transcodeList(rleList: RLElist1): RLElist2 {
+export function* transcode(rleList: RLElist1): RLElist2 {
   for (const el of rleList) {
     if (el instanceof Array) {
       if (el[1] > 2) {
@@ -104,9 +105,7 @@ export function* transcodeList(rleList: RLElist1): RLElist2 {
 /**
  * Encode regular list of integers (not RLElist) into an RLElist2
  */
-export function* encode(
-  list: IterableIterator<number>
-): RLElist2 {
+export function* encode2(list: IterableIterator<number>): RLElist2 {
   // we start off with regular array because we dont know how long it will be
   let reps = 0
   let nextNum: number
@@ -127,31 +126,30 @@ export function* encode(
 
 /**
  * Encode successive diffs of a list of integers into an RLElist2
+ * note: you need to remember the first value yourself
  */
-export function encodeDiffs(list: Iterable<number>): RLElist2{
-  return encode(diffs(list))
+export function encode2Diffs(list: Iterable<number>): RLElist2 {
+  return encode2(diffs(list))
 }
 
-
-export function decodedList2Length(rleList: RLElist2): number {
-  let len = 1 // we count the start value!
+export function decode2Length(rleList: RLElist2): number {
+  let len = 0 // we count the start value!
   for (const el of rleList) {
     if (el) len++
-      else {
-      const reps = rleList.next().value() - 1
+    else {
+      const reps = rleList.next().value - 1
       len += reps
     }
   }
   return len
 }
 
-
-export function* decodeList2(rleList: RLElist2): IterableIterator<number> {
+export function* decode2(rleList: RLElist2): IterableIterator<number> {
   for (const el of rleList) {
     if (el) yield el
     else {
-      const reps = rleList.next().value()
-      const repeated = rleList.next().value()
+      const reps = rleList.next().value
+      const repeated = rleList.next().value
       for (let j = 0; j < reps; j++) {
         yield repeated
       }
@@ -159,14 +157,89 @@ export function* decodeList2(rleList: RLElist2): IterableIterator<number> {
   }
 }
 
-
 /**
  * Decode a RLEList2 of diffs into the original integer values
  */
-export function decodeDiffList2(
+export function decode2Diffs(
   rleList: RLElist2,
   firstValue = 0
 ): IterableIterator<number> {
-  const decodedDiffs = decodeList2(rleList)
+  const decodedDiffs = decode2(rleList)
   return cumulativeSum(decodedDiffs, firstValue)
+}
+
+/*
+ * ****** Unit tests *******
+ */
+
+import { DEV_BUNDLE } from "../../Env"
+if (DEV_BUNDLE) doUnitTests()
+
+type lizt = IterableIterator<number> | number[]
+function equal(list1: lizt, list2: lizt): boolean {
+  const iter1 = Array.isArray(list1)? list1[Symbol.iterator]() : list1
+  const iter2 = Array.isArray(list2)? list2[Symbol.iterator]() : list2
+  let done1: boolean
+  let done2: boolean
+  while (!done1 && !done2){
+    const next1 = iter1.next()
+    const next2 = iter2.next()
+    const v1 = next1.value
+    const v2 = next2.value
+    if (v1 !== v2) return false
+    done1 = next1.done
+    done2 = next2.done
+  }
+  if (!done1 || !done2) return false
+  return true
+}
+
+function doUnitTests(): void {
+  const log = console.log
+  log("Unit Testing StreamRLE")
+
+  const list = [0, 1, 4, 5, 6, 7, 9]
+  const diffs = [1, 3, 1, 1, 1, 2]
+  const rle1EncodedDiffs: RLElist1 = [1, 3, [1, 3], 2]
+  const rle2EncodedDiffs = [1, 3, 0, 3, 1, 2]
+
+  // cumulativeSum test
+  const summedDiffs = cumulativeSum(diffs, 0)
+  if (!equal(summedDiffs, list)) throw "bad cumulativeSum"
+
+  // length test
+  if (decode1Length(rle1EncodedDiffs) !== diffs.length)
+    throw "bad decode1Length"
+
+  // decodeList1 test
+  const decoded1 = decode1(rle1EncodedDiffs)
+  if (!equal(decoded1, diffs)) throw "bad decode1"
+
+  // decodeDiffList1 test
+  const diffDecoded1 = decode1Diffs(rle1EncodedDiffs, 0)
+  if (!equal(diffDecoded1, list)) throw "bad decode1Diffs"
+
+  // transcode test
+  const transcoded = transcode(rle1EncodedDiffs)
+  if (!equal(transcoded, rle2EncodedDiffs)) throw "bad transcode"
+
+  // encode2
+  const encoded2 = encode2(diffs.values())
+  if (!equal(encoded2, rle2EncodedDiffs)) throw "bad encode2"
+
+  // encode2Diffs
+  const encoded22 = encode2Diffs(list)
+  if (!equal(encoded22, rle2EncodedDiffs)) throw "bad encode2Diffs"
+
+  // decode2Length
+  if (decode2Length(rle2EncodedDiffs.values()) !== diffs.length)
+    throw "bad decode2Length"
+
+  // decode2
+  const decoded2 = decode2(rle2EncodedDiffs.values())
+  if (!equal(decoded2, diffs)) throw "bad decode2"
+
+  // decode2Diffs
+  const diffDecoded2 = decode2Diffs(rle2EncodedDiffs.values(), 0)
+  if (!equal(diffDecoded2, list)) throw "bad decode2Diffs"
 }
