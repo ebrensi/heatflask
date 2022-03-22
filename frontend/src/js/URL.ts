754305c0f1601383772b5694985a87269ea26912
Efrem-ASUS-N56V <barefootefrem@gmail.com>
2022-03-22 01:26:50 -0700

    fix small bug

--- frontend/src/js/URL.ts
+++ frontend/src/js/URL.ts
@@ -91,184 +91,181 @@ export function parseURL() {
     : undefined
 
   if (queryType) {
-    query = {queryType: queryType}
+    query = { queryType: queryType }
 
     const qt = query.queryType
-    if ((qt === "days") && urlParams.days)
-      query.quantity = +urlParams.days
-    else if ((qt === "activities") && urlParams.limit)
+    if (qt === "days" && urlParams.days) query.quantity = +urlParams.days
+    else if (qt === "activities" && urlParams.limit)
       query.quantity = +urlParams.limit
     else if (qt === "dates") {
       query.before = urlParams.before
       query.after = urlParams.after
-    } else if (qt === "ids")
-      query.ids = urlParams.ids
-    else if (qt === "key")
-      query.key = urlParams.key
-   else {
-     query = DefaultQuery
-   }
-
+    } else if (qt === "ids") query.ids = urlParams.ids
+    else if (qt === "key") query.key = urlParams.key
+    else {
+      query = DefaultQuery
+    }
+  }
   const endpoint = window.location.pathname.substring(1)
   query.userid = endpoint
-
-  // TODO: continue here
-
-  /*
-   * Construct Visual from URL args
-   */
-  const visual = { ...DefaultVisual }
-
-  const vParamsInit: VisualParameters = {
-    center: { lat: params["lat"], lng: params["lng"] },
-    zoom: params["zoom"],
-    geohash: params["geohash"],
-    autozoom: bool(params["autozoom"]),
-    paused: bool(params["paused"]),
-    baselayer: params["baselayer"],
-
-    s: NaN,
-    tau: params["tau"],
-    T: params["T"],
-    sz: params["sz"],
-    alpha: params["alpha"],
-    shadows: bool(params["shadows"]),
-    paths: bool(params["paths"]),
-  }
-
-  return {
-    qParams: qParamsInit,
-    vParams: vParamsInit,
-  }
 }
+// TODO: continue here
 
-export type QueryObject = QueryParameters & BoundObject
+//   /*
+//    * Construct Visual from URL args
+//    */
+//   const visual = { ...DefaultVisual }
 
-const qParams: QueryObject = BoundObject.fromObject(qParamsInit, {
-  event: "change",
-})
+//   const vParamsInit: VisualParameters = {
+//     center: { lat: params["lat"], lng: params["lng"] },
+//     zoom: params["zoom"],
+//     geohash: params["geohash"],
+//     autozoom: bool(params["autozoom"]),
+//     paused: bool(params["paused"]),
+//     baselayer: params["baselayer"],
 
-const afterDateElement: HTMLInputElement =
-  document.querySelector("[data-bind=after]")
-const beforeDateElement: HTMLInputElement =
-  document.querySelector("[data-bind=before]")
+//     s: NaN,
+//     tau: params["tau"],
+//     T: params["T"],
+//     sz: params["sz"],
+//     alpha: params["alpha"],
+//     shadows: bool(params["shadows"]),
+//     paths: bool(params["paths"]),
+//   }
 
-qParams.onChange("after", (newDate) => {
-  beforeDateElement.min = newDate
-})
+//   return {
+//     qParams: qParamsInit,
+//     vParams: vParamsInit,
+//   }
+// }
 
-qParams.onChange("before", (newDate) => {
-  afterDateElement.max = newDate
-})
+// export type QueryObject = QueryParameters & BoundObject
 
-/*
- * qArgs are updated on render and vArgs are updated every time the map moves
- */
-let url, qArgs
+// const qParams: QueryObject = BoundObject.fromObject(qParamsInit, {
+//   event: "change",
+// })
 
-function initializeURL() {
-  qArgs = {}
-  switch (qParams.queryType) {
-    case "activities":
-      qArgs.n = qParams.quantity
-      break
+// const afterDateElement: HTMLInputElement =
+//   document.querySelector("[data-bind=after]")
+// const beforeDateElement: HTMLInputElement =
+//   document.querySelector("[data-bind=before]")
 
-    case "days":
-      qArgs.days = qParams.quantity
-      break
+// qParams.onChange("after", (newDate) => {
+//   beforeDateElement.min = newDate
+// })
 
-    case "ids":
-      qArgs.id = qParams.ids
-      break
+// qParams.onChange("before", (newDate) => {
+//   afterDateElement.max = newDate
+// })
 
-    case "dates":
-      if (qParams.before) qArgs.before = qParams.before
-      if (qParams.after) qArgs.after = qParams.after
-      break
+// /*
+//  * qArgs are updated on render and vArgs are updated every time the map moves
+//  */
+// let url, qArgs
 
-    case "key":
-      qArgs.key = qParams.key
-  }
+// function initializeURL() {
+//   qArgs = {}
+//   switch (qParams.queryType) {
+//     case "activities":
+//       qArgs.n = qParams.quantity
+//       break
 
-  // if (qParams.userid) qArgs.user = qParams.userid;
+//     case "days":
+//       qArgs.days = qParams.quantity
+//       break
 
-  updateURL()
-}
+//     case "ids":
+//       qArgs.id = qParams.ids
+//       break
 
-/*
- * Reset qParams back to the values from the last render (in qArgs).
- */
-export function resetQuery() {
-  if ("n" in qArgs) {
-    qParams.queryType = "activities"
-    qParams.quantity = qArgs.n
-  } else if ("days" in qArgs) {
-    qParams.queryType = "days"
-    qParams.quantity = qArgs.days
-  } else if ("id" in qArgs) {
-    qParams.queryType = "ids"
-    qParams.ids = qArgs.id
-  } else if ("before" in qArgs) {
-    qParams.queryType = "dates"
-    qParams.before = qArgs.before
-    qParams.after = qArgs.after
-  } else if ("key" in qArgs) {
-    qParams.queryType = "key"
-    qParams.key = qArgs.key
-  }
-}
+//     case "dates":
+//       if (qParams.before) qArgs.before = qParams.before
+//       if (qParams.after) qArgs.after = qParams.after
+//       break
 
-/*
- * If the user closes the sidebar without performing the query, the
- * query form resets to the last query.
- */
-sidebar.addEventListener("closing", resetQuery)
+//     case "key":
+//       qArgs.key = qParams.key
+//   }
 
-export function getUrlString(altQargs) {
-  const vArgs = {}
+//   // if (qParams.userid) qArgs.user = qParams.userid;
 
-  // put geohash in the url if autozoom is disabled
-  if (!vParams.autozoom) vArgs.geohash = vParams.geohash
+//   updateURL()
+// }
 
-  // include baselayer name if it isn't the default
-  const blName = vParams.baselayer.name
-  if (blName !== defaultBaselayerName) vArgs.map = blName
+// /*
+//  * Reset qParams back to the values from the last render (in qArgs).
+//  */
+// export function resetQuery() {
+//   if ("n" in qArgs) {
+//     qParams.queryType = "activities"
+//     qParams.quantity = qArgs.n
+//   } else if ("days" in qArgs) {
+//     qParams.queryType = "days"
+//     qParams.quantity = qArgs.days
+//   } else if ("id" in qArgs) {
+//     qParams.queryType = "ids"
+//     qParams.ids = qArgs.id
+//   } else if ("before" in qArgs) {
+//     qParams.queryType = "dates"
+//     qParams.before = qArgs.before
+//     qParams.after = qArgs.after
+//   } else if ("key" in qArgs) {
+//     qParams.queryType = "key"
+//     qParams.key = qArgs.key
+//   }
+// }
 
-  // include boolean options as 1 or 0 if they differ from the defaults
-  for (const param of ["paused", "shadows", "paths"]) {
-    const val = vParams[param],
-      defaultVal = urlArgDefaults[param][1]
-    if (val !== defaultVal) {
-      vArgs[param] = val ? 1 : 0
-    }
-  }
+// /*
+//  * If the user closes the sidebar without performing the query, the
+//  * query form resets to the last query.
+//  */
+// sidebar.addEventListener("closing", resetQuery)
 
-  // include the global alpha value if it differs from 1
-  for (const param of ["T", "tau", "alpha", "sz"]) {
-    const val = vParams[param],
-      defaultVal = urlArgDefaults[param][1]
-    if (val !== defaultVal) {
-      vArgs[param] = Math.round(val)
-    }
-  }
+// export function getUrlString(altQargs) {
+//   const vArgs = {}
 
-  const paramsString = Object.entries({ ...(altQargs || qArgs), ...vArgs })
-    .map(([k, v]) => `${k}=${encodeURIComponent(v)}`)
-    .join("&")
-  return `?${paramsString}`
-}
+//   // put geohash in the url if autozoom is disabled
+//   if (!vParams.autozoom) vArgs.geohash = vParams.geohash
 
-async function updateURL() {
-  await nextTask()
-  const newURL = getUrlString()
+//   // include baselayer name if it isn't the default
+//   const blName = vParams.baselayer.name
+//   if (blName !== defaultBaselayerName) vArgs.map = blName
 
-  if (url !== newURL) {
-    // console.log(`pushing: ${newURL}`);
-    url = newURL
-    window.history.replaceState("", "", newURL)
-  }
-}
+//   // include boolean options as 1 or 0 if they differ from the defaults
+//   for (const param of ["paused", "shadows", "paths"]) {
+//     const val = vParams[param],
+//       defaultVal = urlArgDefaults[param][1]
+//     if (val !== defaultVal) {
+//       vArgs[param] = val ? 1 : 0
+//     }
+//   }
 
-initializeURL()
-vParams.onChange(updateURL)
-map.on("moveend", updateURL)
+//   // include the global alpha value if it differs from 1
+//   for (const param of ["T", "tau", "alpha", "sz"]) {
+//     const val = vParams[param],
+//       defaultVal = urlArgDefaults[param][1]
+//     if (val !== defaultVal) {
+//       vArgs[param] = Math.round(val)
+//     }
+//   }
+
+//   const paramsString = Object.entries({ ...(altQargs || qArgs), ...vArgs })
+//     .map(([k, v]) => `${k}=${encodeURIComponent(v)}`)
+//     .join("&")
+//   return `?${paramsString}`
+// }
+
+// async function updateURL() {
+//   await nextTask()
+//   const newURL = getUrlString()
+
+//   if (url !== newURL) {
+//     // console.log(`pushing: ${newURL}`);
+//     url = newURL
+//     window.history.replaceState("", "", newURL)
+//   }
+// }
+
+// initializeURL()
+// vParams.onChange(updateURL)
+// map.on("moveend", updateURL)
